5e333f1365553114292915d110462cda168ed4a6
Ursuzev Boris <BorerKicker@yandex.ru>
2022-08-10 18:18:54 +0300

    added CLIENT

---
+++ CLIENT/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это ваш клиент");
+            OurClient ourClient = new OurClient();
+        }
+    }
+}
